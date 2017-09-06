1bb3a65764634658d12a024b4b52d96bc6731092
Zameer Haque <haque864@gmail.com>
2017-09-06 23:50:41 +0530

    Implemented quicksort for SPT

--- asign4/shortest_processing_time.c
+++ asign4/shortest_processing_time.c
@@ -8,12 +8,42 @@ struct process{
     int bt;
 };
 
+struct process p[1000000];
+
+int partition(int low,int high){
+	int i,j;
+	struct process pivot,temp;
+	pivot=p[high];
+	i=low-1;
+	for(j=low;j<=high-1;j++){
+		if(p[j].bt<pivot.bt){
+			i=i+1;
+			temp=p[i];
+			p[i]=p[j];
+			p[j]=temp;
+		}
+	}
+
+	temp=p[i+1];
+	p[i+1]=p[high];
+	p[high]=temp;
+	return i+1;
+}
+
+void quicksort(int low,int high){
+	if(low<high){
+		int q;
+		q=partition(low,high);
+		quicksort(low,q-1);
+		quicksort(q+1,high);
+	}
+}
+
 int main()
 {   
     int no_of_process,i,new_process,j;
     printf("Enter the number of process\n");
     scanf("%d",&no_of_process);
-	struct process p[no_of_process],temp;
 	int wt[no_of_process],tat[no_of_process];
 	float wtavg,tatavg;
     for(i=0;i<no_of_process;i++){
@@ -24,16 +54,8 @@ int main()
         p[i].pid = new_process;
         p[i].bt = rand()%100;
         kill(new_process,SIGKILL);
-    }
-	for(i=0;i<no_of_process-1;i++){
-		for(j=0;j<no_of_process-1-i;j++){
-			if(p[j].bt>p[j+1].bt){
-				temp=p[j+1];
-				p[j+1]=p[j];
-				p[j]=temp;
-			}
-		}
 	}
+	quicksort(0,no_of_process-1);
 	wt[0]=wtavg=0;
 	tat[0]=tatavg=p[0].bt;
 	for(i=1;i<no_of_process;i++){
